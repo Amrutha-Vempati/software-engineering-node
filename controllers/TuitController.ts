59d9ce88a6e71c3027f0cd371216e5439237f6b4
Amrutha-Vempati <amrutha.vempati@gmail.com>
2022-02-09 18:50:06 -0800

    Daos and Controllers

---
+++ controllers/TuitController.ts
@@ -0,0 +1,44 @@
+import {Request, Response, Express} from "express";
+import TuitDao from "../daos/TuitDao";
+import TuitControllerI from "../interfaces/TuitController";
+
+
+export default class TuitController implements TuitControllerI{
+
+    app: Express;
+    tuitDao: TuitDao;
+    constructor(app: Express, tuitDao: TuitDao) {
+        this.app = app;
+        this.tuitDao = tuitDao;
+        this.app.get('/tuits', this.findAllTuits);
+        this.app.get('/tuits/:tid', this.findAllTuits);
+        this.app.get('/tuits/:uid/tuits', this.findTuitsByUser);
+        this.app.post('/tuits', this.createTuit);
+        this.app.delete('/tuits/:tid', this.deleteTuit);
+        this.app.put('/tuits/:tid', this.updateTuit);
+    }
+
+    findAllTuits = (req: Request, res: Response) =>
+        this.tuitDao.findAllTuits()
+            .then(tuit => res.json(tuit));
+
+    findTuitById = (req: Request, res: Response) =>
+        this.tuitDao.findTuitById(req.params.tid)
+            .then(tuit => res.json(tuit));
+
+    findTuitsByUser = (req: Request, res: Response) =>
+        this.tuitDao.findTuitsByUser(req.params.uid)
+            .then(tuit => res.json(tuit));
+
+    createTuit = (req: Request, res: Response) =>
+        this.tuitDao.createTuit(req.body)
+            .then(tuit => res.json(tuit));
+
+    updateTuit = (req: Request, res: Response) =>
+        this.tuitDao.updateTuit(req.params.tid, req.body)
+            .then(status => res.json(status));
+
+    deleteTuit = (req: Request, res: Response) =>
+        this.tuitDao.deleteTuit(req.params.tid)
+            .then(status => res.json(status));
+}
