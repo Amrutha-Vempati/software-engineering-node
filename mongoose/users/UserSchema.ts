b72f4109fd480b5e5a6986c85451665ce4041ddc
Amrutha-Vempati <amrutha.vempati@gmail.com>
2022-02-25 12:36:15 -0800

    added JSDoc

--- mongoose/users/UserSchema.ts
+++ mongoose/users/UserSchema.ts
@@ -21,10 +21,10 @@
 import mongoose from "mongoose";
 import User from "../../models/users/User";
 const UserSchema = new mongoose.Schema<User>({
-    //username: {type: String, required: true, default: `testusername${Date.now()}`},
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-    //password: {type: String, required: true, default: `testpassword${Date.now()}`},
+    username: {type: String, required: true, default: `testusername${Date.now()}`},
+    // username: {type: String, required: true},
+    // password: {type: String, required: true},
+    password: {type: String, required: true, default: `testpassword${Date.now()}`},
     firstName: String,
     lastName: String,
     email: {type: String, required: true, default: `testemail${Date.now()}`},
